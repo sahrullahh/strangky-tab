5004de0124e2eddabdfe0885e215bd61de461278
sahrullahh <49519441+sahrullahh@users.noreply.github.com>
2024-07-19 17:41:21 +0700

    successfully integrated tailwindcss as default page styling

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,34 @@
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import nunjucks from 'vite-plugin-nunjucks';
+import nunjucks from "vite-plugin-nunjucks";
 
 export default defineConfig({
-    root: "src",
-    build: {
-        outDir: "../dist",
-        emptyOutDir: true,
-        target: "es2020",
-        rollupOptions: {
-            input: {
-                main: "./src/index.html",
-                popup: "./src/popup.html",
-            },
-        },
+  root: "src",
+  base: "./",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    target: "es2020",
+    rollupOptions: {
+      input: {
+        main: "./src/index.html",
+        popup: "./src/popup.html",
+      },
     },
-    plugins: [
-        nunjucks(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'manifest.json',
-                    dest: '.',
-                },
-                {
-                    src: 'js/background.js',
-                    dest: '.',
-                }
-            ],
-        }),
-    ],
+  },
+  plugins: [
+    nunjucks(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: ".",
+        },
+        {
+          src: "js/background.js",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
 });
